5c4216574965679fff11a2b4a7b52c28b83ca7fa
Goce I <124073351+giliev560@users.noreply.github.com>
2023-02-28 17:15:06 +0000

    Loop to make sure x>=y

--- main.sh
+++ main.sh
@@ -5,6 +5,15 @@ read x
 echo "Enter whole number y, y < x"
 read y 
 
+while (( x < y ))
+do
+   echo "x needs to be larger than y, please enter the numbers again"
+   echo "Enter whole number x"
+   read x
+   echo "Enter whole number y, y<=x"
+   read y
+done   
+
 div() {
   result=$((x / y))
   if [[ $result =~ ^[0-9]+$ ]] & [[ $((result * y)) == $x ]]
